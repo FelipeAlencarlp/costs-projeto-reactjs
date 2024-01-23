cd0668aff6058fcc3f2ad46e097403a9fa504efe
Felipe <felipealencar.0014@gmail.com>
2024-01-23 18:38:49 -0300

    Criação de componentes, estilização página Home

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import {  BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </ul>
+      <Navbar />
 
       <Container customClass="minHeigth">
         <Routes>
           <Route path="/" element={<Home />}/> 
           <Route path="/Company" element={<Company />}/> 
+          <Route path="/Projects" element={<Projects />}/> 
           <Route path="/Contact" element={<Contact />}/> 
           <Route path="/NewProject" element={<NewProject />}/>   
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
