c17a2989865d2dab1b51df910b282eb7df31c0ae
Felipe <felipealencar.0014@gmail.com>
2024-01-24 10:55:37 -0300

    criado formulário para novo adicionar projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,15 @@
+import ProjectForm from '../project/ProjectForm';
+
+import styles from './NewProject.module.css';
+
 function NewProject() {
     return (
-        <h1>NewProject</h1>
+        <div className={styles.newProjectContainer}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços.</p>
+            
+            <ProjectForm />
+        </div>
     );
 }
 
